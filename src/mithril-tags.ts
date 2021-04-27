2ed3d5a8413fe32cbcd9f52838ad8b0ab72ad850
Pierce Vidderhorn <pierce@vidderhorn.com>
2021-04-27 03:15:01 -0700

    rewrote

--- src/mithril-tags.ts
+++ src/mithril-tags.ts
@@ -1,29 +1,43 @@
 import * as mithril from "mithril";
 
-type Tags = { [E in keyof JSX.IntrinsicElements]: BoundHyperscript & Classes };
-type Classes = { [cc: string]: BoundHyperscript }
-
-interface BoundHyperscript {
-  (...children: mithril.Children[]): mithril.Vnode<any, any>;
-  (attributes: mithril.Attributes, ...children: mithril.Children[]): mithril.Vnode<any, any>;
-}
+export type Tags = { [E in keyof JSX.IntrinsicElements]: TagsElement };
+export type TagsElement = { [cssClass: string]: TagsElementWithClass };
+export type TagsElementWithClass = { [anotherCssClass: string]: TagsElementWithClass };
+export type TagsStatic = { kebabize: boolean };
 
 const tagCache = {};
+const mithrilKeys = Object.keys(mithril);
 
-export const m = new Proxy(mithril, {
-  get: (obj, tag) => {
-    if (blacklist.indexOf(tag as string) > -1) {
-      return mithril[tag];
-    }
-    if (!tagCache.hasOwnProperty(tag)) {
-      tagCache[tag] = new Proxy(mithril.bind(null, tag), {
-        get: (obj, cc) => mithril.bind(null, `${tag.toString()}.${cc.toString()}`)
-      });
-    }
-    return tagCache[tag];
+export const m = new Proxy(mithril, { get: tagsElement }) as mithril.Static & Tags & TagsStatic;
+m.kebabize = true;
+
+function tagsElement(obj: any, tag: string): TagsElement {
+  if (mithrilKeys.indexOf(tag) > -1) {
+    return mithril[tag];
   }
-}) as mithril.Static & Tags;
+  if (!tagCache.hasOwnProperty(tag)) {
+    tagCache[tag] = new Proxy(mithril.bind(null, tag), { get: tagsElementWithClass });
+  }
+  return tagCache[tag];
+
+  function tagsElementWithClass(obj: any, cssClass: string) {
+    if (m.kebabize) cssClass = camelToKebab(cssClass);
+    const proxy = new Proxy(mithril.bind(null, `${tag}.${cssClass}`), { get: tagsElementWithAnotherClass });
+    Object.defineProperty(proxy, "tagsClasses", { value: [cssClass], enumerable: false });
+    return proxy;
+  }
+
+  function tagsElementWithAnotherClass(obj: { tagsClasses: string[] }, cssClass: string) {
+    if (m.kebabize) cssClass = camelToKebab(cssClass);
+    const classes = [...obj.tagsClasses, cssClass];
+    const proxy = new Proxy(mithril.bind(null, `${tag}.${classes.join(".")}`), { get: tagsElementWithAnotherClass });
+    Object.defineProperty(proxy, "tagsClasses", { value: classes, enumerable: false });
+    return proxy;
+  }
+}
+
+function camelToKebab(camel: string) {
+  return camel.replace(/[A-Z]/g, c => "-" + c.toLowerCase());
+}
 
 export default m;
-
-const blacklist = Object.keys(mithril);
--- tests/mithril-tags.test.ls
+++ tests/mithril-tags.test.ls
@@ -1,7 +1,8 @@
 { JSDOM } = require "jsdom"
+
 dom = new JSDOM!
 global.window = dom.window
 global.request-animation-frame = dom.request-animation-frame
 { m } = require "../src/mithril-tags.ts"
 
-console.log m.div.test.it "hello"
+console.log m.div.foo.bar-and-baz "hello!"
